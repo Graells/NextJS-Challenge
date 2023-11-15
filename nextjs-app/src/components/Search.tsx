b469d01dd6fda71e5a706ebbcb02bd3ddaf1afef
Xavier Graells <fxgraells@outlook.com>
2023-11-15 20:00:46 +0100

    feat: onKeyDown html funcionality + reset input after user selects

--- nextjs-app/src/components/Search.tsx
+++ nextjs-app/src/components/Search.tsx
@@ -29,11 +29,22 @@ export default function Search({ onSelectAddress }: SearchProps) {
         if (place.formatted_address) {
           console.log('Formatted address:', place.formatted_address);
           onSelectAddress(place.formatted_address);
+          if (inputRef.current) {
+            inputRef.current.value = ''; // Reset input
+          }
         }
       });
     };
     initializeAutocomplete();
   }, [onSelectAddress]);
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && inputRef.current) {
+      const searchQuery = inputRef.current.value;
+      onSelectAddress(searchQuery);
+      inputRef.current.value = ''; // Reset input
+    }
+  };
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -46,6 +57,7 @@ export default function Search({ onSelectAddress }: SearchProps) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder="Address"
         required
+        onKeyDown={handleKeyPress}
       />
       {/* <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
     </div>
