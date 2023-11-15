15343104aac99fce065577e1084ed5e413e0b265
Xavier Graells <fxgraells@outlook.com>
2023-11-15 19:36:30 +0100

    feat: add /map route using App Router

---
+++ nextjs-app/src/app/map/layout.tsx
@@ -0,0 +1,8 @@
+export default function MapLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section>
+      <nav>brickbro layout</nav>
+      {children}
+    </section>
+  );
+}
