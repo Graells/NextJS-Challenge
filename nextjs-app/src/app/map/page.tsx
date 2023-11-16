31d98d3d867d469ac399535c1ba939f9c79dcbeb
Xavier Graells <fxgraells@outlook.com>
2023-11-16 09:40:43 +0100

    fix: style fix search width, feat: user can select searches

--- nextjs-app/src/app/map/page.tsx
+++ nextjs-app/src/app/map/page.tsx
@@ -4,14 +4,21 @@ import Mapa from '@/components/Mapa';
 import Search from '@/components/Search';
 import { useSearch } from '@/contexts/SearchContext';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function MapPage() {
   const { searches, addSearch } = useSearch();
-  const recentSearch = searches[0] || 'Barcelona';
+  const [selectedAddress, setSelectedAddress] = useState(
+    searches[0] || 'Barcelona',
+  );
 
   const handleSearch = (searchQuery: string) => {
     addSearch(searchQuery);
+    setSelectedAddress(searchQuery);
     console.log('searchesToContext', searches);
+  };
+  const handleSelectAddress = (address: string) => {
+    setSelectedAddress(address);
   };
   return (
     <div>
@@ -21,16 +28,21 @@ export default function MapPage() {
         width={300}
         height={77.73}
         className="ml-16 mb-8 mt-4"
+        priority
       ></Image>
       <div className="ml-16 mb-8 mr-16">
         <Search onSelectAddress={handleSearch} />
-        <Mapa address={recentSearch} />
+        <Mapa address={selectedAddress} />
       </div>
       <div className="border-2 border-gray-300 rounded-md mr-16 ml-16 mb-16 p-6">
         <h2 className=" font-bold text-gray-500 mb-3">Búsquedas</h2>
         <ul>
           {searches.map((search, index) => (
-            <li className="mb-2" key={index}>
+            <li
+              className="mb-2 cursor-pointer"
+              key={index}
+              onClick={() => handleSelectAddress(search)}
+            >
               {search}
             </li>
           ))}
